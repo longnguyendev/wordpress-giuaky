0e72529d3650e9976b8122ed49438bba524450ae
hongtudev <hongtu218@gmail.com>
2023-10-24 19:12:15 +0700

    module-3

--- wordpress/wp-content/themes/twentytwentyone/template-parts/footer/footer-widgets.php
+++ wordpress/wp-content/themes/twentytwentyone/template-parts/footer/footer-widgets.php
@@ -8,10 +8,12 @@
  */
 
 if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-
+	<div class="module-3">
 	<aside class="widget-area">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	</aside><!-- .widget-area -->
 
+	</div>
+	
 	<?php
 endif;
