6c9382edd4a0a71e8a02527067059504100e31c6
Long Nguyễn Thành <longnguyenthanh@Longs-MacBook-Pro.local>
2023-11-01 20:06:09 +0700

    fix fuction wp_trim_words

--- wordpress/wp-content/themes/twentytwentyone/index.php
+++ wordpress/wp-content/themes/twentytwentyone/index.php
@@ -21,82 +21,78 @@ get_header(); ?>
 		<div class="favorite"></div>
 		<div>Bài viết mới nhất</div>
 	</div>
-	
+
 	<div class="row ">
 		<div class="col-md-4 module-11">
-				<div class="row edit-view">
-					<?php $query = new WP_Query(array('orderby' => 'date', 'posts_per_page' => 8));
-					if ($query->have_posts()) {
-						$count = 1;
-						while ($query->have_posts()) {
-							$query->the_post();
-							$title = get_the_title();
-							// Hiển thị thông tin bài viết
-							echo '<div class="col-6">';
-							echo '<div class="row border-top border-bot">';
-							echo '<div class="col-2 post-count">' . $count++ . '</div>';
-							echo '<a href="' . get_permalink() . '" class="col-10 py-2 post-title">' . $title . '</a>';
-							echo '</div>';
-							echo '</div>';
-						}
-						wp_reset_postdata();
-					}
-					?>
-				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="module-2">
-					<?php
-					// Lấy danh sách các bài viết từ widget "Latest Posts"
-					$recent_posts = wp_get_recent_posts(
-						array(
-							'numberposts' => 5,
-							// Số lượng bài viết bạn muốn hiển thị
-							'post_status' => 'publish'
-						)
-					);
-
-					// Duyệt qua danh sách bài viết và hiển thị thông tin
-					foreach ($recent_posts as $post) {
-						echo '<div class ="module-2 post-card row" >';
-						
-						echo '<div class="post-crated-at col-md-3 text-center">';
-						echo '<p class="h1">' . date('d', strtotime($post['post_date'])) . '</p>';
-						echo '<p>Tháng' . date('m', strtotime($post['post_date'])) . '</p>';
-						echo '</div>'; // Thời gian đăng bài viết
-						echo '<h4 class="post-title col-md-9"><a href="' . get_permalink($post['ID']) . '">' . $post['post_title'] . '</a>'; // Tiêu đề bài viết
-						if (strlen($post['post_content']) > 152) {
-							echo '<p class="content">' . substr($post['post_content'], 0, 152) . '<a href="'.get_permalink($post['ID']).'">[...]</a>' . '</p>'; // Nội dung bài viết
-						} else {
-							echo '<p class="content">' . $post['post_content'] . '</p>'; // Nội dung bài viết
-						}
-						echo '</h4>';
+			<div class="row edit-view">
+				<?php $query = new WP_Query(array('orderby' => 'date', 'posts_per_page' => 8));
+				if ($query->have_posts()) {
+					$count = 1;
+					while ($query->have_posts()) {
+						$query->the_post();
+						$title = get_the_title();
+						// Hiển thị thông tin bài viết
+						echo '<div class="col-6">';
+						echo '<div class="row border-top border-bot">';
+						echo '<div class="col-2 post-count">' . $count++ . '</div>';
+						echo '<a href="' . get_permalink() . '" class="col-10 py-2 post-title">' . $title . '</a>';
+						echo '</div>';
 						echo '</div>';
 					}
-					?>
-				</div>
-
-			</div>
-			<div class="col-md-2">
-				<div class="module-12">
-					<?php
-					$has_sidebar_2 = is_active_sidebar('sidebar-3');
-
-					if ($has_sidebar_2) { ?>
-
-						<div class="footer-widgets column-two grid-item">
-							<?php dynamic_sidebar(index: 'sidebar-3'); ?>
-						</div>
-
-					<?php }
-					?>
-
-				</div>
+					wp_reset_postdata();
+				}
+				?>
 			</div>
 		</div>
+		<div class="col-md-6">
+			<div class="module-2">
+				<?php
+				// Lấy danh sách các bài viết từ widget "Latest Posts"
+				$recent_posts = wp_get_recent_posts(
+					array(
+						'numberposts' => 5,
+						// Số lượng bài viết bạn muốn hiển thị
+						'post_status' => 'publish'
+					)
+				);
+
+				// Duyệt qua danh sách bài viết và hiển thị thông tin
+				foreach ($recent_posts as $post) {
+					echo '<div class ="module-2 post-card row" >';
+
+					echo '<div class="post-crated-at col-md-3 text-center">';
+					echo '<p class="h1">' . date('d', strtotime($post['post_date'])) . '</p>';
+					echo '<p>Tháng' . date('m', strtotime($post['post_date'])) . '</p>';
+					echo '</div>'; // Thời gian đăng bài viết
+					echo '<h4 class="post-title col-md-9"><a href="' . get_permalink($post['ID']) . '">' . $post['post_title'] . '</a>'; // Tiêu đề bài viết
+					echo '<p class="content">' . wp_trim_words($post['post_content'], 55, '<a href="' . get_permalink($post['ID']) . '"> [...]</a>' . '</p>') . '</p>'; // Nội dung bài viết
+					echo '</h4>';
+					echo '</div>';
+				}
+				?>
+			</div>
+
+		</div>
+		<div class="col-md-2">
+			<div class="module-12">
+				<?php
+				$has_sidebar_2 = is_active_sidebar('sidebar-3');
+
+				if ($has_sidebar_2) { ?>
+
+					<div class="footer-widgets column-two grid-item">
+						<?php dynamic_sidebar(index: 'sidebar-3'); ?>
+					</div>
+
+				<?php }
+				?>
+
+			</div>
+		</div>
+	</div>
 </div>
 
-<?php if (is_home() && !is_front_page() && !empty(single_post_title('', false))): ?>
+<?php if (is_home() && !is_front_page() && !empty(single_post_title('', false))) : ?>
 	<header class="page-header alignwide">
 		<h1 class="page-title">
 			<?php single_post_title(); ?>
--- wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
+++ wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
@@ -20,24 +20,20 @@
 		$content = explode(' ', get_the_content(), 15);
 		$images = get_the_post_thumbnail(get_the_ID());
 		echo '<div class ="module-5 post-card row border-content" >';
-			echo '<div class="row">';
-				echo '<div class="col-md-4 img-content">';
-					echo $images;
-				echo '</div>';
-				echo '<div class="col-md-2 my-3 text-center post-crated-at">';
-					echo '<div class="daymonth">';
-						echo '<span><h1>' . $day . '</h1></span>';
-						echo '<span><p> Tháng ' . $month . '</p></span>';
-					echo '</div>'; 
-				echo '</div>'; 
-				echo '<h4 class="col-md-6 pt-3 post-title"><a class="title-content" href="' . get_permalink() . '">' . get_the_title() . '</a>'; // Tiêu đề bài viết
-					if (count($content) >= 15) {
-						echo '<p class="content">' . wp_trim_words(get_the_content(), 15, '') . '<a href="' . get_permalink() . '"> [...]</a>' . '</p>'; // Nội dung bài viết
-					} else {
-						echo '<p class="content">' . get_the_content() . '</p>';
-					}
-				echo '</h4>';
-			echo '</div>';
+		echo '<div class="row">';
+		echo '<div class="col-md-4 img-content">';
+		echo $images;
+		echo '</div>';
+		echo '<div class="col-md-2 my-3 text-center post-crated-at">';
+		echo '<div class="daymonth">';
+		echo '<span><h1>' . $day . '</h1></span>';
+		echo '<span><p> Tháng ' . $month . '</p></span>';
+		echo '</div>';
+		echo '</div>';
+		echo '<h4 class="col-md-6 pt-3 post-title"><a class="title-content" href="' . get_permalink() . '">' . get_the_title() . '</a>'; // Tiêu đề bài viết
+		echo '<p class="content">' . wp_trim_words(get_the_content(), 55, '<a href="' . get_permalink() . '"> [...]</a>' . '</p>'); // Nội dung bài viết
+		echo '</h4>';
+		echo '</div>';
 		echo '</div>';
 		?>
 	</div>
